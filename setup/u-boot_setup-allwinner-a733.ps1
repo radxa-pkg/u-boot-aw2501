eff05ec4a5c701942514552c0dc6c78206b2fe06
ZHANG Yuntian <yt@radxa.com>
2025-08-05 10:08:46 +0000

    fix: add A733 setup scripts
    A733 has different offset for SPI bootloader.

---
+++ setup/u-boot_setup-allwinner-a733.ps1
@@ -0,0 +1,58 @@
+#!/usr/bin/env pwsh
+
+using namespace System.IO
+
+$ErrorActionPreference = "Stop"
+
+Set-Variable ERROR_REQUIRE_FILE -Option Constant -Value -3
+Set-Variable ERROR_ILLEGAL_PARAMETERS -Option Constant -Value -4
+Set-Variable ERROR_REQUIRE_TARGET -Option Constant -Value -5
+
+Function BuildSPINOR{
+	$output = [FileStream]::new("spi.img", [FileMode]::Create, [FileAccess]::ReadWrite)
+
+    Write-Host "Warning: GPT partitioning is currently unimplemented."
+
+    $boot0 = [Directory]::GetFiles($PSScriptRoot, "boot0_spinor.bin");
+	$input = [FileStream]::new($boot0[0], [FileMode]::Open, [FileAccess]::Read)
+	$output.Seek(0, [SeekOrigin]::Begin)
+	$input.CopyTo($output)
+	$input.Close()
+
+	$input = [FileStream]::new("$PSScriptRoot/boot_package.fex", [FileMode]::Open, [FileAccess]::Read)
+	$output.Seek(512 * 512, [SeekOrigin]::Begin)
+	$input.CopyTo($output)
+	$input.Close()
+
+	$input = [FileStream]::new("$PSScriptRoot/sys_partition_nor.bin", [FileMode]::Open, [FileAccess]::Read)
+	$output.Seek(512 * 3296, [SeekOrigin]::Begin)
+	$input.CopyTo($output)
+	$input.Close()
+
+	$output.SetLength(8MB)
+	$output.Close()
+    Write-Host "SPI U-Boot has been created as spi.img under the current directory."
+}
+
+$ret = 0
+switch ($args[0]) {
+    "BuildSPINOR" {
+        $ret = BuildSPINOR
+    }
+    "" {
+        Write-Host "An operation is required.
+
+Supported operations:
+        BuildSPINOR
+"
+        $ret = $ERROR_ILLEGAL_PARAMETERS
+    }
+    default {
+        Write-Host "$_ is not a supported operation!"
+        $ret = $ERROR_ILLEGAL_PARAMETERS
+    }
+}
+
+if ($ret -ne 0) {
+    exit $ret
+}
---
+++ setup/u-boot_setup-allwinner-a733.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+if [[ ! -v ERROR_REQUIRE_FILE ]]; then
+	readonly ERROR_REQUIRE_FILE=-3
+fi
+if [[ ! -v ERROR_ILLEGAL_PARAMETERS ]]; then
+	readonly ERROR_ILLEGAL_PARAMETERS=-4
+fi
+if [[ ! -v ERROR_REQUIRE_TARGET ]]; then
+	readonly ERROR_REQUIRE_TARGET=-5
+fi
+
+build_spinor() {
+	rm -f /tmp/spi.img /tmp/gpt.img
+	truncate -s 8M /tmp/spi.img
+	dd conv=notrunc,fsync if="$SCRIPT_DIR/boot0_spinor.bin" of=/tmp/spi.img bs=512
+	dd conv=notrunc,fsync if="$SCRIPT_DIR/boot_package.fex" of=/tmp/spi.img bs=512 seek=512
+	dd conv=notrunc,fsync if="$SCRIPT_DIR/sys_partition_nor.bin" of=/tmp/spi.img bs=512 seek=3296
+}
+
+update_bootloader() {
+	local DEVICE=$1
+
+	dd conv=notrunc,fsync if="$SCRIPT_DIR/boot0_sdcard.bin" of="$DEVICE" bs=512 seek=256
+	dd conv=notrunc,fsync if="$SCRIPT_DIR/boot_package.fex" of="$DEVICE" bs=512 seek=24576
+	sync "$DEVICE"
+}
+
+erase_bootloader() {
+	local DEVICE=$1
+
+	dd conv=notrunc,fsync if=/dev/zero of="$DEVICE" bs=512 seek=256 count=1
+	sync "$DEVICE"
+}
+
+erase_emmc_boot() {
+	if [[ -f "/sys/class/block/$(basename "$1")/force_ro" ]]; then
+		echo 0 >"/sys/class/block/$(basename "$1")/force_ro"
+	fi
+	blkdiscard -f "$@"
+}
+
+erase_spinor() {
+	local DEVICE=${1:-/dev/mtd0}
+
+	if [[ ! -e $DEVICE ]]; then
+		echo "$DEVICE is missing." >&2
+		return "$ERROR_REQUIRE_TARGET"
+	fi
+
+	flash_erase "$DEVICE" 0 0
+}
+
+update_spinor() {
+	local DEVICE=${1:-/dev/mtd0}
+
+	if [[ ! -e $DEVICE ]]; then
+		echo "$DEVICE is missing." >&2
+		return "$ERROR_REQUIRE_TARGET"
+	fi
+
+	build_spinor
+	erase_spinor "$DEVICE"
+	echo "Writing to $DEVICE..."
+	flashcp /tmp/spi.img "$DEVICE"
+	rm /tmp/spi.img
+	sync
+}
+
+# https://stackoverflow.com/a/28776166
+is_sourced() {
+	if [ -n "$ZSH_VERSION" ]; then
+		case $ZSH_EVAL_CONTEXT in
+		*:file:*)
+			return 0
+			;;
+		esac
+	else # Add additional POSIX-compatible shell names here, if needed.
+		case ${0##*/} in
+		dash | -dash | bash | -bash | ksh | -ksh | sh | -sh)
+			return 0
+			;;
+		esac
+	fi
+	return 1 # NOT sourced.
+}
+
+if ! is_sourced; then
+
+	set -euo pipefail
+	shopt -s nullglob
+
+	SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+	ACTION="$1"
+	shift
+
+	if [[ $(type -t "$ACTION") == function ]]; then
+		$ACTION "$@"
+	else
+		echo "Unsupported action: '$ACTION'" >&2
+		exit "$ERROR_ILLEGAL_PARAMETERS"
+	fi
+
+fi
